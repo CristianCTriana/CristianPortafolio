7025ff05dacc035438585b59591c49d04ded1d66
Camilo T <cristianctr97@gmail.com>
2022-08-29 19:36:28 -0500

    practicasComponentAndFooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,34 @@
 import '../styles/Footer.css';
+import {FaGithub, FaLinkedin, FaWhatsapp} from 'react-icons/fa';
+import {GoMail} from 'react-icons/go';
+import ReactTooltip from 'react-tooltip';
 
 function Footer() {
+
+  const iconStyles = {
+    fontSize: "20px",
+    marginRight: "5px"
+  }
+
+  const tagAStyles = {
+    textDecoration: "none",
+    color: "white",
+    cursor: "pointer"
+  }
+
   return (
-    <div>
-      Hola soy el footer
+    <div className='Footer'>
+      <div className='Contact'>
+        <div className='Networks'>
+          <a href='https://www.linkedin.com/in/cristianc-trianar/' style={tagAStyles} target="_blank" rel="noopener noreferrer"><span className='info'><FaLinkedin style={iconStyles}/>Linkedin</span></a>
+          <a href='https://github.com/CristianaCTriana' style={tagAStyles} target="_blank" rel="noopener noreferrer"><span className='info'><FaGithub style={iconStyles}/>GitHub</span></a>
+        </div>
+        <div className='WriteMe'>
+          <a data-tip="Es posible que se abra la app de Whatsapp al hacer clic" href='https://wa.me/573058502605' style={tagAStyles} target="_blank" rel="noopener noreferrer"><span className='info'><FaWhatsapp style={iconStyles}/>+57 3058502605</span></a>
+          <a style={tagAStyles} target="_blank" rel="noopener noreferrer"><span className='info'><GoMail style={iconStyles}/>cristianctr97@gmail.com</span></a>
+          <ReactTooltip effect='solid'/>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Practicas.jsx
+++ src/components/Practicas.jsx
@@ -1,9 +1,84 @@
 import '../styles/Practicas.css';
+import { useEffect } from 'react';
+import { FaCss3, FaEye, FaHashtag, FaHtml5, FaReact } from "react-icons/fa";
+import { SiJavascript, SiRedux } from "react-icons/si";
+import ReactTooltip from 'react-tooltip';
+import copyWriteChallenge from '../Imgs/copyWriteChallenge.png';
 
 function Practicas() {
+let slideIndex = 1;
+  const slides = document.getElementsByClassName('SlideP');
+
+  let showSildes = (n)=>{
+    let i;
+    let dots = document.getElementsByClassName('DotP');
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    if (slides.length !== 0) slides[slideIndex-1].style.display = "flex";  
+    if (dots.length !== 0) dots[slideIndex-1].className += " active";
+  }
+
+  useEffect(()=>{
+    showSildes(slideIndex);
+  },[])
+
+  let plusSlides = (n)=>{
+    showSildes( slideIndex += n);
+    console.log("click")
+  }
+
+  let currentSlide = (n) => {
+    showSildes(slideIndex = n)
+  }
+
   return (
     <div className='Practicas' id='Practicas'>
-      Practicas Components
+
+      <h2 className='PracticasTittle'>Prácticas</h2>
+
+      <div><span className='TextP'>Aqui hay pequeños proyectos que uso para practicar.</span></div>
+
+      <div className='SlidesContainerP'>
+
+        <div className='SlideP Fade'>
+          <div className='ImagesContainerP'>
+            <img src={copyWriteChallenge} className='SlideImgP'></img>
+          </div>
+          <div className='TextContainerP'>
+            <div className='TittleP'><span><strong>Palabras Palíndromas</strong></span></div>
+            <div className='TextP'>App que te muestra una palabra ingresa por el usuario escrita al revés y te muestra si es o no palíndroma.</div>
+            <div className='TecnologiesP'>
+              <FaReact data-tip="ReactJs" className='IconP'/>
+              <SiRedux data-tip="Redux" className='IconP'/>
+              <FaHtml5 data-tip="Html" className='IconP'/>
+              <FaCss3 data-tip="CSS3" className='IconP'/>
+              <SiJavascript data-tip="JavaScript" className='IconP'/>
+              <ReactTooltip effect='solid' />
+              <ReactTooltip effect='solid' />
+            </div>
+            <div className='ButtonsP'>
+              <a className='ButtonCodeP' href='https://github.com/CristianaCTriana/Copywrite-Challenge' target="_blank" rel="noopener noreferrer"><FaHashtag style={{marginRight: '8px'}}/>Código</a>
+              <a className='ButtonCodeP' href='https://challenge-copywrite-camilot.herokuapp.com' target="_blank" rel="noopener noreferrer"><FaEye style={{marginRight: '8px'}}/>Ver</a>
+              <ReactTooltip effect='solid' />
+            </div>
+          </div>
+        </div>
+
+        <a className='PrevP' onClick={()=>plusSlides(-1)}>❮</a>
+        <a className='NextP' onClick={()=>plusSlides(1)}>❯</a>
+
+      </div>
+
+      <div className='DotContainerP'>
+        <span className='DotP' onClick={() => currentSlide(1)}></span>
+        {/* <span className='DotP' onClick={() => currentSlide(2)}></span> */}
+      </div>
     </div>
   );
 }
