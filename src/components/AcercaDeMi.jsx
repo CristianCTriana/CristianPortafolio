7054eec82e1f0693f73f4c49ccde80c083e9b69f
Camilo T <cristianctr97@gmail.com>
2022-08-29 20:15:35 -0500

    nameCorrect

--- src/components/AcercaDeMi.jsx
+++ src/components/AcercaDeMi.jsx
@@ -5,7 +5,7 @@ function AcercaDeMi() {
     <div className='AcercaDeMi'>
       <div className='AcercaDeMiContainer'>
         <div className='NombreYDescripcion'>
-          <div className='AcercaDeMiNombre'><h1>Critian Triana</h1></div>
+          <div className='AcercaDeMiNombre'><h1>Cristian Triana</h1></div>
           <div className='AcercaDeMiDescripcion'><span>Desarrollador web especializado en Back-End, énfasis en el uso de Sequelize y JavaScript, actualmente enfocado en reforzar y mantener todos mis conocimientos en cuanto a programación pero también con grandes aspiraciones de desarrollar proyectos mucho más grandes.</span></div>
         </div>
         <div className='FotoYRedes'>
